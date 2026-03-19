f454f6a0be1baed40c5574477e1889f4fa4f2be8
Mohamed-L <mohamedlarson11@gmail.com>
2026-03-19 15:58:27 -0400

    post turn smoothing change

--- src/helpers.h
+++
@@ -1,8 +0,0 @@
-#ifndef HELPERS_H
-#define HELPERS_H
-
-void runMotors(int deltaL, int deltaR);
-void turnOnLED(int colour);
-void toggleLED(int colour);
-
-#endif // HELPERS_H
--- src/lineFollowing.ino
+++ src/lineFollowing.ino
@@ -1,18 +1,20 @@
 #include <Servo.h>
-#include "helpers.h"
 
 #define MAX_DELTA 15
+#define BLACK 2200
+#define WHITE 600
+#define TURN_SPEED 8
+
+#define RED 10
+#define GRN 9
+#define YLW 5
 
 Servo leftWheel;
 Servo rightWheel;
-
 int error;
 int deltaL;
 int deltaR;
 
-extern const int RED = 10;
-extern const int GRN = 9;
-extern const int YLW = 5;
 const int BUTTON = 7;
 
 const int MOTOR_R = 3;
@@ -22,12 +24,13 @@ const int LSENSOR = A1;
 const int RSENSOR = A2;
 const int SHARP = A3;
 
-  int lvalue = 0;  //left sensor value
-  int rvalue = 0;  //right sensor value
-  int distance = 0; //SHARP sensor value
+const int stopPulse = 145;
 
-extern const int stopPulse = 148;  // default = 155
-int Delta = 5;                                                      
+int lvalue = 0;  //left sensor value
+int rvalue = 0;  //right sensor value
+int distance = 0; //SHARP sensor value
+
+int Delta = 8;
 const int offset = 1;
 
 //plugged in values
@@ -35,12 +38,80 @@ const float Kp = 0.0035;
 const float Kd = 0.0000045;
 
 int lastError = 0;
+int turnType = 0;
 
-void setup(){  
+// run robot wheels
+void runMotors(int deltaL, int deltaR)
+{
+  int pulseL = (stopPulse + deltaL)*10;    //length of pulse in microseconds
+  int pulseR = (stopPulse + deltaR)*10;
+  leftWheel.writeMicroseconds(pulseL);
+  rightWheel.writeMicroseconds(pulseR);
+}
+
+void turn(){
+  if (lvalue > BLACK && rvalue > BLACK) {
+    runMotors(TURN_SPEED, TURN_SPEED);
+    delay(200);
+
+    runMotors(TURN_SPEED, -TURN_SPEED);
+    delay(1250);
+
+    runMotors(TURN_SPEED, TURN_SPEED);
+    delay(750);
+
+    runMotors(0, 0);
+    delay(400);
+  }
+  if (distance > 1300) {
+    runMotors(TURN_SPEED, -TURN_SPEED + offset);
+    delay(2750);
+
+    runMotors(TURN_SPEED, TURN_SPEED);
+    delay(650);
+
+    runMotors(0, 0);
+    delay(400);
+  }
+  lastError = 0;
+}
+
+// Turn on a single LED and turn others off
+void turnOnLED(int colour)
+{
+  digitalWrite(GRN, LOW);
+  digitalWrite(YLW, LOW);
+  digitalWrite(RED, LOW);
+  digitalWrite(colour, HIGH);
+}
+
+// Light LED based on error direction
+void led_direction(int error) {
+  if (error > 0) {
+    turnOnLED(RED);
+  } else if (error < 0) {
+    turnOnLED(GRN);
+  } else {
+    turnOnLED(YLW);
+  }
+}
+
+//Toggle an LED on/off
+void toggleLED(int colour){
+  digitalWrite(GRN, LOW);
+  digitalWrite(YLW, LOW);
+  digitalWrite(RED, LOW);
+  digitalWrite(colour, HIGH);
+  delay(250);
+  digitalWrite(colour, LOW);
+  delay(250);
+}
+
+void setup(){
   pinMode(GRN, OUTPUT);
   pinMode(YLW, OUTPUT);
   pinMode(RED, OUTPUT);
-  
+
   pinMode(BUTTON, INPUT_PULLUP);
 
   pinMode(LSENSOR, INPUT);
@@ -49,8 +120,8 @@ void setup(){
 
   leftWheel.attach(MOTOR_L);
   rightWheel.attach(MOTOR_R);
-  Serial.begin(9600);     
-  
+  Serial.begin(9600);
+
   while(digitalRead(BUTTON) == LOW){
     digitalWrite(GRN, HIGH);
     delay(125);
@@ -67,7 +138,7 @@ void loop() {
   distance = map(analogRead(SHARP), 0, 1023, 0, 3300);
 
   error = lvalue - rvalue;
-  
+
   float derivative = error - lastError;
   float correction = (Kp * error) + (Kd * derivative);
 
@@ -81,13 +152,7 @@ void loop() {
   deltaR = constrain(deltaR, 0, MAX_DELTA);
 
   led_direction(error);
-
-  Serial.println(distance);
-  if(distance >= 1300){
-    deltaL = 0;
-    deltaR = 0;
-  }
+  turn();
 
   runMotors(deltaL, deltaR);
 }
-
